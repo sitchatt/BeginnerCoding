ffa40adbaf51277d1b44a767949ff1416ad9ff9c
sitchatt <89836369+sitchatt@users.noreply.github.com>
2021-09-17 20:26:35 +0530

    Add files via upload

---
+++ Folder Create.ps1
@@ -0,0 +1,7 @@
+﻿ $foldername1 = Read-Host "Enter Folder 1 Name"
+$foldername2 = Read-Host "Enter Folder 2 Name"
+$filename = Read-Host "Enter File Name"
+New-Item -Path "C:\CodeTest" -ItemType Directory -Name $foldername1
+New-Item -Path "C:\CodeTest" -ItemType Directory -Name $foldername2
+New-Item -Path "C:\CodeTest\$foldername1\" -ItemType File -Name $filename
+Copy-Item "C:\CodeTest\$foldername1\$filename" "C:\CodeTest\$foldername2"
